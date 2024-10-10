13c4875f26b078dc921ad2f6a138174aa0611fa7
skywalker2017 <1394375146@qq.com>
2024-10-10 13:06:32 +0800

    video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ function Inner() {
     }
   }, [debug]);
 
-  const predictUrl = async () => {
+  /* const predictUrl = async () => {
 
     try {
       const response = await fetch('/api/gradio', {
@@ -66,6 +66,50 @@ function Inner() {
       setResponse(`http://127.0.0.1:6006/file=/root/autodl-tmp/Linly-Talker/temp/${hash}/myface_answer.mp4`);
     } catch (error) {
       console.error('Error submitting data:', error);
+    }
+  } */
+
+  const fetchVideoWithHeaders = async (url: string) => {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        "ngrok-skip-browser-warning": "69420",// Add your custom header here
+      }
+    });
+    console.log("hello here")
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const blob = await response.blob();
+    return URL.createObjectURL(blob);
+  };
+
+  const predictUrl = async () => {
+
+    const response = await fetch('/api/gradio', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ inputValue }),
+    });
+
+    /* const result = await response.json();
+    const url = result.message;
+    const tail = url.split('temp/')[1];
+    const hash = tail.split('/')[0]; */
+    const result = await response.json();
+    const hash = result.message;
+
+    //const videoUrl = `http://127.0.0.1:6006/file=/root/autodl-tmp/Linly-Talker/temp/${hash}/myface_answer.mp4`; // Replace with your video URL
+    const videoUrl = `https://b736-43-143-237-200.ngrok-free.app/${hash}.mp4`
+    try {
+      const blobUrl = await fetchVideoWithHeaders(videoUrl);
+      setResponse(blobUrl);
+    } catch (error) {
+      console.error('Error fetching video:', error);
     }
   }
 
@@ -96,6 +140,7 @@ function Inner() {
                 <source src={response} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
+              <button onClick={predictUrl}>hello</button>
             </div>
           </SDKProvider>
         </AppStateProvider>
